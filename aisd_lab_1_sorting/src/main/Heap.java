b96e0fecbdadd29426c1c0a01f0502e14a117283
Michal Banaszczak <michal.banaszczak.stud@pw.edu.pl>
2021-10-18 15:48:35 +0200

    zajecia 2

---
+++ aisd_lab_1_sorting/src/main/Heap.java
@@ -0,0 +1,11 @@
+package pl.edu.pw.ee;
+
+public class Heap<T extends Comparable<T>> implements HeapInterface<T> {
+    public void put(T item) {
+
+    }
+
+    public T pop() {
+        
+    }
+}
