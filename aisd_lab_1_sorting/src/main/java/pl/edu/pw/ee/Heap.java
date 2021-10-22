272c574a92e918fa54f49fb8cdf36e47cbdd9cf9
Michal Banaszczak <michal.banaszczak.stud@pw.edu.pl>
2021-10-22 21:14:57 +0200

    reworked keeping track of heaps length

--- aisd_lab_1_sorting/src/main/java/pl/edu/pw/ee/Heap.java
+++ aisd_lab_1_sorting/src/main/java/pl/edu/pw/ee/Heap.java
@@ -5,39 +5,35 @@ import java.util.ArrayList;
 
 public class Heap<T extends Comparable<T>> implements HeapInterface<T> {
     private ArrayList<T> heap;
-    private int n;
 
     public Heap() {
         heap = new ArrayList<T>();
-        n = 0;
     }
 
     public void put(T item) {
         heap.add(item);
-        n++;
         
         heapUp();
     }
 
     public T pop() {
-        if (n <= 0) {
+        if (heap.size() <= 0) {
             throw new IndexOutOfBoundsException("Tried to pop from empty heap."); 
         }
-        swap(heap, 0, (n--)-1);
+        swap(0, heap.size() - 1);        
 
         heapDown();
 
         printheap();
-        System.out.println(n);
-        return heap.get(n);
+        return heap.remove(heap.size() - 1);
     }
 
     public void heapUp() {
-        int currentIndex = n - 1;
+        int currentIndex = heap.size() - 1;
         int parentIndex = (currentIndex - 1) / 2;
 
         while (heap.get(parentIndex).compareTo(heap.get(currentIndex)) < 0 && currentIndex != parentIndex) {
-            swap(heap, parentIndex, currentIndex);
+            swap(parentIndex, currentIndex);
             currentIndex = parentIndex;
             parentIndex = (currentIndex - 1) / 2;
         }
@@ -47,23 +43,23 @@ public class Heap<T extends Comparable<T>> implements HeapInterface<T> {
         int currentIndex = 0;
         int childIndex = currentIndex * 2 + 1;
 
-        while (childIndex < n) {
-            if (childIndex + 1 < n && heap.get(childIndex).compareTo(heap.get(childIndex + 1)) < 0)
+        while (childIndex < heap.size() - 1) {
+            if (childIndex + 1 < heap.size() - 1 && heap.get(childIndex).compareTo(heap.get(childIndex + 1)) < 0)
                 childIndex++;
             if (heap.get(currentIndex).compareTo(heap.get(childIndex)) >= 0)
                 break;
             
-            swap(heap, currentIndex, childIndex);
+            swap(currentIndex, childIndex);
             currentIndex = childIndex;
             childIndex = currentIndex * 2 + 1;
         }
     }
 
-    private void swap(ArrayList<T> data, int firstId, int secondId) {
+    private void swap(int firstId, int secondId) {
         if (firstId != secondId) {
-            T firstValue = data.get(firstId);
-            data.set(firstId, data.get(secondId));
-            data.set(secondId, firstValue);
+            T firstValue = heap.get(firstId);
+            heap.set(firstId, heap.get(secondId));
+            heap.set(secondId, firstValue);
         }
     }
 
