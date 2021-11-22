d557c1af7cf48fbddc05f98d2e1d230d52a948ba
Michal Banaszczak <michal.banaszczak.stud@pw.edu.pl>
2021-11-22 15:47:11 +0100

    rbt 2

--- aisd_lab5_rbt/src/test/java/pl/edu/pw/ee/RbtMapTest.java
+++ aisd_lab5_rbt/src/test/java/pl/edu/pw/ee/RbtMapTest.java
@@ -1,6 +1,21 @@
 package pl.edu.pw.ee;
 
+import pl.edu.pw.ee.services.MapInterface;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
 public class RbtMapTest {
-// TODO
+    @Test
+    public void test0() {
+        MapInterface<String, String> mapa = new RbtMap<String, String>();
+        mapa.setValue("key", "value");
+        System.out.println(mapa.getValue("key"));
+        // ???
+    }
 
 }
