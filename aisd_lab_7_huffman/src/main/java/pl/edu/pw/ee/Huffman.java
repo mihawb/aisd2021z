5040a46fe43e453ce4d71f1b3a2aad464904882c
Michal Banaszczak <michal.banaszczak.stud@pw.edu.pl>
2021-11-29 15:48:43 +0100

    lab 7 huffman

---
+++ aisd_lab_7_huffman/src/main/java/pl/edu/pw/ee/Huffman.java
@@ -0,0 +1,20 @@
+package pl.edu.pw.ee;
+
+public class Huffman {
+
+    class Node {
+        private int freq;
+        private char sign;
+        private Node left;
+        private Node right;
+    }
+
+    public int huffman(String pathToRootDir, boolean compress){
+        // pathToRootDir = sciezka do folderu
+        // w folderze ma sie znajdowac jeden plik o zdefiniowanej nazwie zawierajacy tekst zrodlowy lub bit po kopresji
+        // compress decyduje czy zwracamy ilosc bitow po kompresji czy ilosc liter w pliku zrodlowym
+
+    	// TODO
+    	return -1;
+    }
+}
