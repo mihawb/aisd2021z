48305462d96bcaf89b95b46a67c74806ca3622c5
Michal Banaszczak <michal.banaszczak.stud@pw.edu.pl>
2021-12-20 15:46:57 +0100

    lab 10 mst poczatek

---
+++ aisd_lab_10_min_spannig_tree/src/main/java/pl/edu/pw/ee/PrimAlgorithm.java
@@ -0,0 +1,15 @@
+package pl.edu.pw.ee;
+
+import pl.edu.pw.ee.services.MinSpanningTree;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class PrimAlgorithm implements MinSpanningTree {
+    public String findMST(String pathToFile){
+        // TODO
+        return null;
+    }
+}
